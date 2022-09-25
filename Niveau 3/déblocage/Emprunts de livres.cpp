cdd840caef21e09df1f435e691074ad5d9e7b345
Haroun Jilani <114358939+HarounJilani@users.noreply.github.com>
2022-09-25 21:26:27 +0100

    Update Emprunts de livres.cpp

--- Niveau 3/déblocage/Emprunts de livres.cpp
+++ Niveau 3/déblocage/Emprunts de livres.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int nbj,nbclients,ind,d,nbl;
+    vector<int>res;
+    cin>>nbl>>nbj;
+
+    int T[nbl]={0};
+
+    for(int i=0;i<nbj;i++){
+        cin>>nbclients;
+        for(int j=0;j<nbclients;j++){
+            cin>>ind>>d;
+            if(T[ind]==0){
+                T[ind]=i+d;
+                res.push_back(1);
+                }
+            else if(T[ind]!=0 && i>=T[ind])
+                {res.push_back(1);
+                T[ind]=d+i;
+                }
+            else if(T[ind]!=0 && i<T[ind])
+                res.push_back(0);
+
+        }
+    }
+    for(int i=0;i<res.size();i++)
+        cout<<res[i]<<endl;
+}
